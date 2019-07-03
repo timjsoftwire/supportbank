fb447d739b74d87e3dbbfabab629812cca323bb2
TimJsoftwire <Tim.Jepsen@softwire.com>
2019-07-03 17:02:41 +0100

    Supportbank now works

--- supportbank.js
+++ supportbank.js
@@ -45,7 +45,7 @@ class Account {
             else { 
                 acc = acc + parseFloat(element[0].amount);
             }})
-        return acc;
+        return acc.toFixed(2);
     }
 
     getAllTransactions() {
@@ -58,8 +58,8 @@ class Account {
 
 
 
- //adds the function to the account, or creates a new one if necessary
- function GiveAccountTransaction(name, isFrom, newTransaction) {
+ //adds a transaction to the relevant account, or creates a new one if necessary
+ function AddTransactionToAccount(name, isFrom, newTransaction) {
     var relevantAccount = Accounts.find(element => (element.name === name));
     if (!relevantAccount){
         addNecessaryAccount(isFrom, name, newTransaction);
@@ -97,63 +97,66 @@ function getTransactionsFromFile (filename) {
     }
 }
 
-
-
-
-
-
-
-function startup(filename) { //TODO: tidy up this function
+function startup(filename) {
     //go through all transactions in the spreadsheet, make them into Transactions (the class) (and in doing so, add those transactions to the Accounts (creating extra accounts when necessary)).
     allTransactions = getTransactionsFromFile(filename);
     
-
+    //go through all transactions  in the spreadsheet
     for (i = 0; i < allTransactions.length; i++) {
         var currentTransaction = allTransactions[i];
-        //makeTransaction(currentTransaction);
-        //Error handling
-        var transactionAddable = true; //stores whether or not a transaction is written in an acceptable enough way to be added
-        if(typeof currentTransaction[5] !=="undefined") {//is there an extra column
-            logger.debug("The ".concat(i+2,"th row of the spreadsheet has data in its 6th column that wasn't expected - the spreadsheet may be of the wrong format"));
+
+        //add if there are no errors
+        if (SpreadsheetErrorHandler(currentTransaction) === true) {
+            transactionjs.NewTransaction(currentTransaction[0],currentTransaction[1],currentTransaction[2],currentTransaction[3],currentTransaction[4]);
         }
-        for (var j = 0; j<5;j++) {// is anything blank
-            if (currentTransaction[j] ==="") {
-                logger.debug("The cell on the ".concat(i+2,"th row and ",j+1,"th column was unexpectedly empty."));
-                transactionAddable = false;
-            }
-        }
-        if (isNaN(currentTransaction[4])) {//amount NaN
-            logger.debug("The amount on the ".concat(i+2,"th row could not be converted to a number."));
+    }
+    //now that all transactions are in the array of transactions, attach those transactions to accounts.
+    transactionjs.GetAllTransactions().forEach(element =>   {  
+        AddTransactionToAccount(element.from,true, element);
+        AddTransactionToAccount(element.to,false, element);})
+    }
+
+
+function SpreadsheetErrorHandler(currentTransaction) {
+    //Error handling. lots of cases to go through
+    var transactionAddable = true; //stores whether or not a transaction is written in an acceptable enough way to be added
+    if(typeof currentTransaction[5] !=="undefined") {//is there an extra column
+        logger.debug("The ".concat(i+2,"th row of the spreadsheet has data in its 6th column that wasn't expected - the spreadsheet may be of the wrong format"));
+    }
+    for (var j = 0; j<5;j++) {// is anything blank
+        if (currentTransaction[j] ==="") {
+            logger.debug("The cell on the ".concat(i+2,"th row and ",j+1,"th column was unexpectedly empty."));
             transactionAddable = false;
         }
-        if (!moment(currentTransaction[0],'DD/MM/YYYY',true).isValid()) {//is the date a date?
+    }
+    if (isNaN(currentTransaction[4])) {//amount NaN
+        logger.debug("The amount on the ".concat(i+2,"th row could not be converted to a number."));
+        transactionAddable = false;
+    } 
+    else {
+        currentTransaction[4]= currentTransaction[4].toString();
+    }
+    if (!moment(currentTransaction[0],'DD/MM/YYYY',true).isValid()) {//is the date a date?
+        
+        currentTransaction[0] = currentTransaction[0].slice(0,10);
+        if (moment(currentTransaction[0],'YYYY-MM-DD').isValid()) {//if the date is in this other format, change it to the normal one
+            currentTransaction[0] = moment(currentTransaction[0], "YYYY-MM-DD").format('DD/MM/YYYY');
+        } else if (!currentTransaction[0].isNaN && currentTransaction[0] > 35000) {//if the date is in the weird xml format, try to fix it
+            logger.debug("Encounted the weird xml format while trying to read a file.");
+        }
+        else {
             
-            currentTransaction[0] = currentTransaction[0].slice(0,10);
-            if (moment(currentTransaction[0],'YYYY-MM-DD').isValid()) {//if the date is in this other format, change it to the normal one
-                currentTransaction[0] = moment(currentTransaction[0], "YYYY-MM-DD").format('DD/MM/YYYY');
-            } else if (!currentTransaction[0].isNaN && currentTransaction[0] > 35000) {//if the date is in the weird xml format, try to fix it
-                logger.debug("Encounted the weird xml format while trying to read a file.");
-            }
-            else {
-                
-            logger.debug("The date on the ".concat(i+2,"th row could not be converted to a date."));
-            transactionAddable = false;
-            }
+        logger.debug("The date on the ".concat(i+2,"th row could not be converted to a date."));
+        transactionAddable = false;
         }
+    }
 
-        if (!transactionAddable) {
-            logger.debug("As such, the transaction on the ".concat(i+2, "th row could not be added.\n"));
-            continue;
-        }
-        transactionjs.NewTransaction(currentTransaction[0],currentTransaction[1],currentTransaction[2],currentTransaction[3],currentTransaction[4]);
-        }
-
-        //now that all transactions are in the array of transactions, attach those transactions to accounts.
-        transactionjs.GetAllTransactions().forEach(element =>   {  
-            GiveAccountTransaction(element.from,true, element);
-            GiveAccountTransaction(element.to,false, element);})
+    if (!transactionAddable) {
+        logger.debug("As such, the transaction on the ".concat(i+2, "th row could not be added.\n"));
+    }
+    return(transactionAddable);
 }
 
-Import_File("Transactions2014.csv");
+Import_File("Transactions2012.xml");
 
 ListAll();
--- xmlreader.js
+++ xmlreader.js
@@ -1,32 +1,25 @@
-/*const fs = require("fs");
+const fs = require("fs");
 const xml2js = require('fast-xml-parser');
 
 
 
 exports.getTransactions = function(filename) {
-    var fileOutput = fs.readFileSync("filename","utf8");
+    var fileOutput = fs.readFileSync(filename,"utf8");
     var transactions = [];
     var lastDatePosition = 0;
     var unparsedfile = xml2js.parse(fileOutput);
     for (i = 0; i< unparsedfile.TransactionList.SupportTransaction.length;i++) {
         transaction = []
-        NextDatePosition = FindNextDatePosition(lastDatePosition);
+        NextDatePosition = fileOutput.indexOf("Date=", lastDatePosition);
         date = fileOutput.slice(NextDatePosition+5,NextDatePosition+11)
-        transaction.push(date)
-        transaction.push(unparsedfile.TransactionList.SupportTransaction[i].Parties.From);
-        transaction.push(unparsedfile.TransactionList.SupportTransaction[i].Parties.To);
-        transaction.push(unparsedfile.TransactionList.SupportTransaction[i].Description);
-        transaction.push(unparsedfile.TransactionList.SupportTransaction[i].Value);
-        
-        transactions.push(transaction);
+
+        unparsedCurrentTransaction = unparsedfile.TransactionList.SupportTransaction[i];
+        transactions.push([date,unparsedCurrentTransaction.Parties.From, unparsedCurrentTransaction.Parties.To, unparsedCurrentTransaction.Description, unparsedCurrentTransaction.Value]);
+
 
         lastDatePosition = NextDatePosition+5;
     }
+    return transactions;
 }
 
 
-function FindNextDatePosition (lastDatePosition) {
-    return fileOutput.indexOf("Date=", lastDatePosition);
-}
-
-*/
